57dd2b2ee39166179eb662b90751243fdeae48a4
Andrii Yukhymenko <andriy111224@gmail.com>
2021-07-30 23:30:32 +0300

    Полностью рабочий вариант

--- index.js
+++ index.js
@@ -1,72 +1,17 @@
 "use strict";
 
-// SlideToggle
-let _slideUp = (target, duration = 500) => {
-  if (!target.classList.contains("_slide")) {
-    target.classList.add("_slide");
-    target.style.transitionProperty = "height, margin, padding";
-    target.style.transitionDuration = duration + "ms";
-    target.style.height = target.offsetHeight + "px";
-    target.offsetHeight;
-    target.style.overflow = "hidden";
-    target.style.height = 0;
-    target.style.paddingTop = 0;
-    target.style.paddingBottom = 0;
-    target.style.marginTop = 0;
-    target.style.marginBottom = 0;
-    window.setTimeout(() => {
-      target.hidden = true;
-      target.style.removeProperty("height");
-      target.style.removeProperty("padding-top");
-      target.style.removeProperty("padding-bottom");
-      target.style.removeProperty("margin-top");
-      target.style.removeProperty("margin-bottom");
-      target.style.removeProperty("overflow");
-      target.style.removeProperty("transition-duration");
-      target.style.removeProperty("transition-property");
-      target.classList.remove("_slide");
-    }, duration);
-  }
-};
+/*
+! Для родителя спойлеров пишем атрибут data-spollers
+! Для заголовков слойлеров пишем атрибут data-spoller
+! Если нужно чтобы в блоке открывался только 1 спойлер, добавляем атрибут data-one-spoller
+? Если нужно включать\выключать работу спойлеров на разных размерах экранов
+? пишем параметры ширины и типа брейкпоинта
+* Например:
+* data-spollers="992,max" - спойлеры будут работать только на экранах  < = 992px
+* data-spollers="768,min" - спойлеры будут работать только на экранах  > = 768px
+*/
 
-let _slideDown = (target, duration = 500) => {
-  if (!target.classList.contains("_slide")) {
-    target.classList.add("_slide");
-    if (target.hidden) {
-      target.hidden = false;
-    }
-    let height = target.offsetHeight;
-    target.style.overflow = "hidden";
-    target.style.height = 0;
-    target.style.paddingTop = 0;
-    target.style.paddingBottom = 0;
-    target.style.marginTop = 0;
-    target.style.marginBottom = 0;
-    target.offsetHeight;
-    target.style.transitionProperty = "height, margin, padding";
-    target.style.transitionDuration = duration + "ms";
-    target.style.height = height + "px";
-    target.style.removeProperty("padding-top");
-    target.style.removeProperty("padding-bottom");
-    target.style.removeProperty("margin-top");
-    target.style.removeProperty("margin-bottom");
-    window.setTimeout(() => {
-      target.style.removeProperty("height");
-      target.style.removeProperty("overflow");
-      target.style.removeProperty("transition-duration");
-      target.style.removeProperty("transition-property");
-      target.classList.remove("_slide");
-    }, duration);
-  }
-};
-let _slideToggle = (target, duration = 500) => {
-  if (target.hidden) {
-    return _slideDown(target, duration);
-  } else {
-    return _slideUp(target, duration);
-  }
-};
-
+// SPOLLERS
 const spollersArray = document.querySelectorAll("[data-spollers]");
 if (spollersArray.length > 0) {
   // Получение обычных спойлеров
@@ -75,7 +20,7 @@ if (spollersArray.length > 0) {
     index,
     self
   ) {
-    return item.dataset.spollers.split(",")[0];
+    return !item.dataset.spollers.split(",")[0];
   });
   // Инициализация обычных спойлеров
   if (spollersRegular.length > 0) {
@@ -133,7 +78,6 @@ if (spollersArray.length > 0) {
           return true;
         }
       });
-
       // Событие
       matchMedia.addListener(function () {
         initSpollers(spollersArray, matchMedia);
@@ -142,7 +86,7 @@ if (spollersArray.length > 0) {
     });
   }
 
-  // Инициализация
+  //Инициализация
   function initSpollers(spollersArray, matchMedia = false) {
     spollersArray.forEach((spollersBlock) => {
       spollersBlock = matchMedia ? spollersBlock.item : spollersBlock;
@@ -157,13 +101,12 @@ if (spollersArray.length > 0) {
       }
     });
   }
-
   // Работа с контентом
-  function initSpollerBody(spollersBlock, hideSpollerBody = true) {
+  function initSpollerBody(spollersBlock, hideSpollersBody = true) {
     const spollerTitles = spollersBlock.querySelectorAll("[data-spoller]");
     if (spollerTitles.length > 0) {
       spollerTitles.forEach((spollerTitle) => {
-        if (hideSpollerBody) {
+        if (hideSpollersBody) {
           spollerTitle.removeAttribute("tabindex");
           if (!spollerTitle.classList.contains("_active")) {
             spollerTitle.nextElementSibling.hidden = true;
@@ -175,6 +118,7 @@ if (spollersArray.length > 0) {
       });
     }
   }
+
   function setSpollerAction(e) {
     const el = e.target;
     if (el.hasAttribute("data-spoller") || el.closest("[data-spoller]")) {
@@ -206,12 +150,73 @@ if (spollersArray.length > 0) {
   }
 }
 
-// Для родителя спойлеров пишем атрибут data-spollers
-// Для заголовков спойлеров пишем атрибут data-spoller
-// Если нужно включать/выключать работу спойлеров на разных размерах экранов
-// пишем параметры ширины и типа брейкпоинта.
-// Например:
-// data-spollers="992,max" - спойлеры будут работать только на экранах меньше или равно 992px
-// data-spollers="768,min" - спойлеры будут работать только на экранах больше или равно 768px
-//
-// Если нужно что бы в блоке открывался только один спойлер добавляем атрибут data-one-spoller
+// SlideToggle
+
+// анимированно скрывает объект
+let _slideUp = (target, duration = 500) => {
+  if (!target.classList.contains("_slide")) {
+    target.classList.add("_slide");
+    target.style.transitionProperty = "height, margin, padding";
+    target.style.transitionDuration = duration + "ms";
+    target.style.height = target.offsetHeight + "px";
+    target.offsetHeight;
+    target.style.overflow = "hidden";
+    target.style.height = 0;
+    target.style.paddintTop = 0;
+    target.style.paddingBottom = 0;
+    target.style.marginTop = 0;
+    target.style.marginBottom = 0;
+    window.setTimeout(() => {
+      target.hidden = true;
+      target.style.removeProperty("height");
+      target.style.removeProperty("padding-top");
+      target.style.removeProperty("padding-bottom");
+      target.style.removeProperty("margin-top");
+      target.style.removeProperty("margin-bottom");
+      target.style.removeProperty("overflow");
+      target.style.removeProperty("transition-duration");
+      target.style.removeProperty("transition-property");
+      target.classList.remove("_slide");
+    }, duration);
+  }
+};
+
+// анимированно показывает объект
+let _slideDown = (target, duration = 500) => {
+  if (!target.classList.contains("_slide")) {
+    target.classList.add("_slide");
+    if (target.hidden) {
+      target.hidden = false;
+    }
+    let height = target.offsetHeight;
+    target.style.overflow = "hidden";
+    target.style.height = 0;
+    target.style.paddintTop = 0;
+    target.style.paddingBottom = 0;
+    target.style.marginTop = 0;
+    target.style.marginBottom = 0;
+    target.offsetHeight;
+    target.style.transitionProperty = "height, margin, padding";
+    target.style.transitionDuration = duration + "ms";
+    target.style.height = height + "px";
+    target.style.removeProperty("padding-top");
+    target.style.removeProperty("padding-bottom");
+    target.style.removeProperty("margin-top");
+    target.style.removeProperty("margin-bottom");
+    window.setTimeout(() => {
+      target.style.removeProperty("height");
+      target.style.removeProperty("overflow");
+      target.style.removeProperty("transition-duration");
+      target.style.removeProperty("transition-property");
+      target.classList.remove("_slide");
+    }, duration);
+  }
+};
+
+let _slideToggle = (target, duration = 500) => {
+  if (target.hidden) {
+    return _slideDown(target, duration);
+  } else {
+    return _slideUp(target, duration);
+  }
+};
